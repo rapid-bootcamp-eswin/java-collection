ab8d3dd9418645618f25a34fa4f0d65cd335fbe8
Eswin <winnapitupulu@gmail.com>
2022-12-20 00:37:34 +0700

    belajar java-collection

---
+++ src/iterableCollection/MainImmutable.java
@@ -0,0 +1,24 @@
+package iterableCollection;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainImmutable {
+    public static void main(String[] args) {
+        Person person = new Person("Ardian",
+                Arrays.asList("Coding","Design","Testing"));
+
+        System.out.println(person);
+        // cara salah
+        //person.getHobbies().add("Gamming");
+
+        // cara benar
+        List<String> hobbies = new ArrayList<>(person.getHobbies());
+        hobbies.add("Gamming");
+        person.setHobbies(hobbies);
+        System.out.println(person);
+    }
+}
+
---
+++ src/iterableCollection/MainIterable.java
@@ -0,0 +1,24 @@
+package iterableCollection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainIterable {
+    public static void main(String[] args) {
+        Iterable<String> names = List.of("Ahmad Roni", "Ahmad Syukur", "Ahmad Subagyo", "Ahmad Bootcamp");
+        for(var name: names){
+            System.out.println(name);
+        }
+
+        Iterable<Person> persons = Arrays.asList(
+                new Person(1,"Ahmad Roni","Ciamis"),
+                new Person(2,"Ahmad Syahroni","Jakarta"),
+                new Person(3,"Ahmad Baehaki","Yogyakarta"),
+                new Person(4,"Ahmad Manulang","Medan")
+        );
+
+        for (var peson: persons){
+            System.out.println(peson);
+        }
+    }
+}
